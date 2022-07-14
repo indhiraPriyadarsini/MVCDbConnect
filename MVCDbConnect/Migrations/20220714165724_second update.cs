b5541c22c28569789d458b1feddf92c9d9ae347c
indhiraPriyadarsini <iipriyadarsini26@gmail.com>
2022-07-14 22:41:10 +0530

    Add project files.

---
+++ MVCDbConnect/Migrations/20220714165724_second update.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVCDbConnect.Migrations
+{
+    public partial class secondupdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Depaertment",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Depaertment",
+                table: "Students",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
---
+++ MVCDbConnect/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCDbConnect.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+
+        public string StudentName { get; set; }
+
+        public int Age { get; set; }
+        
+        public int Rank { get; set; }
+    }
+}
